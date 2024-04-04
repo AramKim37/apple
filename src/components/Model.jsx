e49a127bd2954f389584252020b0637baeba660b
AramKim37 <aramkim37@gmail.com>
2024-04-03 19:54:01 -0600

    add model colors

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,6 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { models } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -69,6 +70,21 @@ const Model = () => {
             >
               <View.Port />
             </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModel(item)}
+                  />
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
